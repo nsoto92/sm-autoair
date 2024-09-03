cc2709c66ce021912afe85b08281ea09471bf161
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2024-09-02 23:21:35 -0400

    Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,15 @@
+'use client';
 import { Overlay, Container, Title, Button, Text } from '@mantine/core';
+import { motion } from "framer-motion"
 import style from './Header.module.css';
 
 export function Header() {
+
+  const triggerCall = () => {
+    console.log("Trigger Call");
+    window.open(`tel:+${process.env.NEXT_PUBLIC_PHONE_NUMBER}`);
+  }
+
   return (
     <div className={style.hero}>
       <Overlay
@@ -9,16 +17,31 @@ export function Header() {
         opacity={1}
         zIndex={0}
       />
-      <Container className={style.container} size="md">
+      <motion.div
+        className={style.container}
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01]
+        }}
+      >
         <Title className={style.title}>SM Auto Air</Title>
         <Text className={style.description} size="xl" mt="xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          En nuestro negocio de aire acondicionado automotriz, ofrecemos una amplia gama de servicios para asegurar el funcionamiento óptimo de tu sistema de climatización. Realizamos mantenimiento completo, limpieza profunda del evaporador, y reemplazo de componentes esenciales como el evaporador, compresor, condensador, drier, motor del soplador y abanicos. Nuestro equipo garantiza un diagnóstico preciso para resolver cualquier problema y mejorar los estimados de la competencia. Además, la mayoría de los servicios se completan el mismo día. Todo el trabajo está garantizado por nuestros técnicos certificados y el colegio de técnicos en refrigeración.
         </Text>
 
-        <Button variant="gradient" size="xl" radius="xl" className={style.control}>
-          Get started
+        <Button
+          variant="subtle"
+          size="xl"
+          radius="xl"
+          className={style.control}
+          onClick={triggerCall}
+        >
+          Llamar para cita
         </Button>
-      </Container>
+      </motion.div>
     </div>
   );
 }
